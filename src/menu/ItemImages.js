a146b2191d56312e6493ea11c78d0ddec761c500
Mukesh216 <727721euec095@skcet.ac.in>
2023-12-01 18:43:08 +0530

    vercel file

--- src/menu/ItemImages.js
+++ src/menu/ItemImages.js
@@ -1,18 +1,18 @@
 export const images = [
     { id: 1, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429771/c1_b6hsgo.jpg", alt: "Nature" },
-    { id: 2, src: "src/assets/pexels-the-masked-guy-3644476.jpg", alt: "Architecture" },
+    { id: 2, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701436248/pexels-abhinav-goswami-291528_shk4ba.jpg", alt: "Architecture" },
     { id: 3, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429772/c3_cd9ekj.jpg", alt: "People" },
     { id: 4, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429771/c6_mv7grg.jpg", alt: "Animals" },
     { id: 5, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429770/c4_d714bg.jpg", alt: "Tech" },
     { id: 6, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429773/c9_paltg1.jpg", alt: "Tech" },
     { id: 7, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429774/pexels-this-is-zun-1194030_1_wowy57.jpg", alt: "Tech" },
     { id: 8, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429773/pexels-mareefe-1417945_pxh1mx.jpg", alt: "Tech" },
-    { id: 9, src: "src/assets/pexels-vlada-karpovich-6947293.jpg", alt: "Tech" },
+    { id: 9, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701436361/croissant_dq6ri5.jpg", alt: "Tech" },
     { id: 10, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429771/c8_i5w89q.jpg", alt: "Tech" }
   ];
 
  export  const tofo = [
-    { id: 1, src: "src/assets/c1.jpg", alt: "Nature" },
+    { id: 1, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701436248/c1_wpywea.jpg", alt: "Nature" },
     { id: 2, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429773/c22_tw7r1v.jpg", alt: "Architecture" },
     { id: 3, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429772/c3_cd9ekj.jpg", alt: "People" },
     { id: 4, src: "https://res.cloudinary.com/dfsvudyfv/image/upload/v1701429771/c6_mv7grg.jpg", alt: "Animals" },
