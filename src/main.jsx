48bc780ef6c086dc5bd02eb13e273bddac67cf8f
Mukesh216 <727721euec095@skcet.ac.in>
2023-12-02 19:51:58 +0530

    git ie

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App></App>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
