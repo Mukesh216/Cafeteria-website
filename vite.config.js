8e3016b7753bc43686c6d74592c7b651f19f146b
Mukesh216 <727721euec095@skcet.ac.in>
2023-12-02 19:57:05 +0530

    git

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Cafeteria-website/', // Set the correct base
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '/src': '/Cafeteria-website/src', // Adjust the alias path
+    },
+  },
+});
